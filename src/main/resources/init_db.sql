63fc6d75a344b398390b6231ad763931f5879898
walaniam <mariusz.walania@gmail.com>
2022-05-27 17:04:42 +0200

    init db script

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS ${MYSQL_DB_NAME};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER}'@'%';
 
-insert ignore into weather_station (host, port, path, protocol, location) values ('192.168.0.137', 80, "/", "http", "balcony");
+INSERT IGNORE INTO weather_station (host, port, path, protocol, location) VALUES ('192.168.0.137', 80, "/", "http", "balcony");
