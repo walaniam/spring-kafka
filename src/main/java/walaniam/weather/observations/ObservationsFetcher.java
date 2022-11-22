23ab786216617ec03ff644d62eaebaf9f13fa292
walaniam <mariusz.walania@gmail.com>
2022-11-22 21:02:07 +0100

    cleanup

--- src/main/java/walaniam/weather/observations/ObservationsFetcher.java
+++ src/main/java/walaniam/weather/observations/ObservationsFetcher.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
+//@Component
 public class ObservationsFetcher {
 
     private final WeatherStationClient client;
